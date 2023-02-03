a8327e3e082bbfff10f6e1c93f2f5bbf0d4a3bba
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-03 15:54:02 -0300

    State lift
    centralizamos o state no pai e definimos qual componente define e qual usa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import {useState} from 'react'
+import SeuNome from './components/SeuNome'
 import './App.css';
-import OutraLista from './components/OutraLista';
+import Saudacao from './components/Saudacao';
 
 function App() {
-  
-  const meusItens = ['react', 'vue', 'angular']
+
+  const[nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>Renderização de listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={[]}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome} />
     </div>
   );
 }
