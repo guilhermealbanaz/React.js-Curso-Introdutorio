2efaada6efc49d72d064bc108d580f82851a2d60
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-03 14:32:52 -0300

    Renderização por condição usando State

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import {useState} from 'react'
-
 import './App.css';
-import Evento from './components/Evento'
-import Form from './components/Form'
+import Condicional from './components/Condicional';
 
 function App() {
-  const[number, setNumber] = useState(1);
-  const[number2, setNumber2] = useState(2);
-
-  const changeNumber = () => {
-    setNumber(number + 1)
-  }
-
-  const changeNumber2 = () => {
-    //Previous value
-    setNumber2((prevNumber2) => prevNumber2 + 1)
-    setNumber2((prevNumber2) => prevNumber2 + 2)
-  }
+  
 
   return (
     <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero={1}/>
-      <Form/>
-      <div>
-        <p>O número atual é: {number}</p>
-        <button onClick={changeNumber}>Somar Número</button>
-      </div>
-      <div>
-        <p>O número atual é: {number2}</p>
-        <label id='botão2'>somando 1 e depois somando 2 no novo valor:</label>
-        <button onClick={changeNumber2} id='botão2'>Alterar 2 vezes uma única váriavel</button>
-      </div>
+      <h1>Renderização condicional</h1>
+      <Condicional/>
     </div>
   );
 }
