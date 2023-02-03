4911aa7d675329110401f77bd5ae932167c28a0e
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-03 12:59:10 -0300

    useState, onSubmit and onClick
    alterando 2 vezes a mesma váriavel em 1 função e alterando e repassando dados de um form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {useState} from 'react'
+
 import './App.css';
 import Evento from './components/Evento'
 import Form from './components/Form'
 
 function App() {
+  const[number, setNumber] = useState(1);
+  const[number2, setNumber2] = useState(2);
+
+  const changeNumber = () => {
+    setNumber(number + 1)
+  }
+
+  const changeNumber2 = () => {
+    //Previous value
+    setNumber2((prevNumber2) => prevNumber2 + 1)
+    setNumber2((prevNumber2) => prevNumber2 + 2)
+  }
 
   return (
     <div className="App">
@@ -10,6 +24,15 @@ function App() {
       <Evento numero={1}/>
       <Evento numero={2}/>
       <Form/>
+      <div>
+        <p>O número atual é: {number}</p>
+        <button onClick={changeNumber}>Somar Número</button>
+      </div>
+      <div>
+        <p>O número atual é: {number2}</p>
+        <label id='botão2'>somando 1 e depois somando 2 no novo valor:</label>
+        <button onClick={changeNumber2} id='botão2'>Alterar 2 vezes uma única váriavel</button>
+      </div>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,13 @@
-
+import {useState} from 'react'
 
 function Form(){
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log('Usuário Cadastrado')
+        console.log(`O usuário: ${name} foi cadastrado com a senha: ${password}`)
     }
 
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
@@ -14,7 +16,12 @@ function Form(){
             </h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type='text' placeholder='Digite seu nome'></input>
+                    <label htmlFor='name'>Nome:</label>
+                    <input type='text' id='name' name='name' placeholder='Digite seu nome' onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <div>
+                    <label htmlFor='password'>Senha:</label>
+                    <input type='password' id='password' name='password' placeholder='Digite sua senha' onChange={(e) => setPassword(e.target.value)}></input>
                 </div>
                 <div>
                     <input type='submit' value='Cadastrar'></input>
