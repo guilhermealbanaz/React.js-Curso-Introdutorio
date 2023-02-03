5616e33e2f1c9da7fe602a8e4e5952f2068f97e8
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-03 16:40:02 -0300

    Trabalhando com rotas
    Router(envolve toda a aplicação), Links(muda url), Routes(views), Route(que rota pertence a qual)

---
+++ src/pages/Contato.js
@@ -0,0 +1,11 @@
+
+
+function Contato(){
+    return(
+        <div>
+            <h1>Contato</h1>
+            <p>Welcome</p>
+        </div>
+    )
+}
+export default Contato
---
+++ src/pages/Empresa.js
@@ -0,0 +1,11 @@
+
+
+function Empresa(){
+    return(
+        <div>
+            <h1>Empresa</h1>
+            <p>Welcome</p>
+        </div>
+    )
+}
+export default Empresa
