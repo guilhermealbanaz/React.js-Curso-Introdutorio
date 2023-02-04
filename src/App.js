0c3b2e3b1e17bea212c3745b825393c689e0324a
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-04 12:53:27 -0300

    Estruturação
    estruturação de rotas funcional e estilos por props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layouts/Container'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+      <Link to='/'>Home</Link>
+      <Link to='/contact'>Contact</Link>
+      <Link to='/company'>Company</Link>
+      <Link to='/newproject'>NewProject</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/company' element={<Company />}></Route>
+          <Route exact path='/contact' element={<Contact />}></Route>
+          <Route exact path='/newproject' element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/layouts/Container.js
@@ -0,0 +1,10 @@
+import styles from './Container.module.css'
+
+function Container({children, customClass}){
+    return(
+        <div className={`${styles.container} ${styles[customClass]}`}>
+            {children}
+        </div>
+    )
+}
+export default Container
