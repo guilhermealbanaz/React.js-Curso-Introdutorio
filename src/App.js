2dbe728858ce3ca484918123f5a289810e7e1111
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-04 12:18:46 -0300

    Costs
    projeto completo com intuito de aplicar conceitos práticados em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Contato from './pages/Contato'
-import Empresa from './pages/Empresa'
-import NavBar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
 
 function App() {
-
   return (
-    <Router>
-      <NavBar/>
-      <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/contato" element={<Contato />} />
-          <Route path="/empresa" element={<Empresa />} />
-      </Routes>
-      <Footer />
-    </Router>
-  )
+    <div className="App">
+      <p>Costs</p>
+    </div>
+  );
 }
 
 export default App;
