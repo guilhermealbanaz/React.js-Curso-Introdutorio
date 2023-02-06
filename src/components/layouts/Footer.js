c71780f229b7694be2cecd8f965a8426ebae6ca6
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-06 13:18:54 -0300

    tela Home finalizada

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,15 +5,21 @@ function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
+                <a href="https://www.linkedin.com/in/guilherme-bragato-albanaz-8a22b422b/">
                 <li>
                     <FaLinkedin/>
                 </li>
+                </a>
+                <a href="https://www.linkedin.com/in/guilherme-bragato-albanaz-8a22b422b/">
                 <li>
                     <FaInstagram/>
                 </li>
+                </a>
+                <a href="https://www.linkedin.com/in/guilherme-bragato-albanaz-8a22b422b/">
                 <li>
                     <FaFacebook/>
                 </li>
+                </a>
             </ul>
             <p className={styles.copy_right}>
                 <span>
