0f5d8b22a4ea2685a7b6500bd48cd5cb9f7da8df
guilhermealbanaz <guibragatoalbnz@gmail.com>
2023-02-07 16:07:25 -0300

    Página de criação de projetos e Home prontas

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import styles from './NewProject.module.css'
+import ProjectForm from '../projects/ProjectForm'
+
 function NewProject(){
     return(
-        <h1>NewProject</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adicionar serviços!</p>
+            <ProjectForm btnText='Criar Projeto'/>
+        </div>
     )
 }
 export default NewProject
